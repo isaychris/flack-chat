e6ea2dabb55c382cf87c62cfe2cfaaa5b457482f
isaychris <adaptxsurvive@gmail.com>
2018-08-31 22:52:32 -0700

    added channel selection, message loading and few other fixes

--- README.md
+++ README.md
@@ -2,24 +2,27 @@
 Slack clone powered by Flask and Socket.IO
 
 WIP. Buggy.
-![Image](https://i.imgur.com/fythFjU.png)
+![Image](https://i.imgur.com/GsgvYjf.png)
 
 ### Requirements:  
 https://docs.cs50.net/web/2018/w/projects/1/project1.html
 
 ### Currently Working
-* Real-time messages
+* Real-time messaging
 * Channel creation
 * Display names
-* Online List
-* Channel List
-
+* Online list
+* Channel list
+* Channel selection
+* Loading recent messages
 
 ### TO-DO
-* Channel selection
-* Limit number of messages for each channel to 100
-* Load saved messages from server
-* Press enter to submit messages
+* ~~Channel selection~~
+* ~~Limit number of messages for each channel to 100~~
+* ~~Load saved messages from server~~
+* ~~Press enter to submit messages~~
+* Check for naming duplicates
 * Fix buggy online-list
+* Fix layout
 * Navigation Bar
 * Dark Mode ?
--- application.py
+++ application.py
@@ -9,40 +9,49 @@ socketio = SocketIO(app)
 
 channel_list = ["lobby"]
 online_list = []
-online = 0
+messages = {"lobby": []}
 
 @app.route("/")
 def index():    
-    return render_template("lobby.html", online_list=online_list, channel_list=channel_list)
+    return redirect(url_for('lobby'))
 
-@app.route("/display", )
-def display():
-    return render_template("lobby.html")
-
-@app.route("/lobby", methods=["GET", "POST"])
+@app.route("/c/lobby")
 def lobby():
-    return render_template("lobby.html", online_list=online_list, channel_list=channel_list)
+    return render_template("channel.html", online_list=online_list, channel_list=channel_list, messages=messages["lobby"], current_channel="lobby")
 
+@app.route("/c/<string:name>")
+def channel(name):
+    if name not in channel_list:
+        return "Error"
 
-@socketio.on("sent message")
+    return render_template("channel.html", online_list=online_list, channel_list=channel_list, messages=messages[name], current_channel=name)
+
+@socketio.on("add message")
 def sent(data):
-    print("sent message")
-    emit("new message", {"user": data["user"], "time": data["time"], "message": data["message"]}, broadcast=True)
+    print("message received")
+    messages[data["channel"]].append((data["user"], data["time"], data["message"]))
+    
+    while(len(messages[data["channel"]]) > 100):
+        messages[data["channel"]].pop(0)
+    
+    print(messages[data["channel"]])
+    emit("announce message", {"user": data["user"], "time": data["time"], "message": data["message"], "channel": data["channel"] }, broadcast=True)
 
-@socketio.on("joined")
+@socketio.on("add user")
 def joined(data):
-    print(data['display'] + " has joined!")    
+    print(data['display'] + " has joined!")
     online_list.append(data['display'])
-    emit("update online", {"online": len(online_list), "display": data['display'], "event": "joined"}, broadcast=True)
+    emit("announce online", {"online": len(online_list), "display": data['display'], "event": "add"}, broadcast=True)
 
-@socketio.on("gone")
+@socketio.on("remove user")
 def gone(data):
     print(data['display'] + " has left!")
     online_list.remove(data['display'])
-    emit("update online", {"online": len(online_list), "display": data['display'], "event": "gone"}, broadcast=True)
+    emit("announce online", {"online": len(online_list), "display": data['display'], "event": "remove"}, broadcast=True)
 
 @socketio.on("add channel")
 def add_channel(data):
     print(data['channel'] + " has been added to channel list!")
     channel_list.append(data['channel'])
-    emit("update channels", {"channel": data['channel']}, broadcast=True)
+    messages.setdefault(data['channel'], [])
+    emit("announce channel", {"channel": data['channel']}, broadcast=True)
