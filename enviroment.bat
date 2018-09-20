b3c006c3d2c9e3d33c7a5ae1617b80ae7fb7020d
isaychris <adaptxsurvive@gmail.com>
2018-09-19 22:26:39 -0700

    update

--- enviroment.bat
+++ enviroment.bat
@@ -1,7 +1,7 @@
 @echo %off
 
-echo "Setting FLASK_APP to application.py ..."
-SET FLASK_APP=application.py
+echo "Setting FLASK_APP to wsgi.py ..."
+SET FLASK_APP=wsgi.py
 
 echo "Setting FLASK_DEBUG to 1 ...
 SET FLASK_DEBUG=1
